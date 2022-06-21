108fc9da2fb8accc81175c304cd740c61a171484
s-amano <amahaya0831@gmail.com>
2022-06-21 16:01:15 +0900

    add return folder path

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
-import path from 'path';
-import { BrowserWindow, app, ipcMain, session } from 'electron';
+import path from "path";
+import { BrowserWindow, app, ipcMain, session, dialog } from "electron";
 
-const isDev = process.env.NODE_ENV === 'development';
+const isDev = process.env.NODE_ENV === "development";
 
 if (isDev) {
-  require('electron-reload')(__dirname, {
+  require("electron-reload")(__dirname, {
     electron: path.resolve(
       __dirname,
-      process.platform === 'win32'
-        ? '../node_modules/electron/dist/electron.exe'
-        : '../node_modules/.bin/electron'
+      process.platform === "win32"
+        ? "../node_modules/electron/dist/electron.exe"
+        : "../node_modules/.bin/electron"
     ),
   });
 }
@@ -20,14 +20,32 @@ const createWindows = () => {
     frame: false,
     transparent: true,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
+      preload: path.join(__dirname, "preload.js"),
     },
   });
 
   const consoleWindow = new BrowserWindow({
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
+      preload: path.join(__dirname, "preload.js"),
     },
+  });
+
+  ipcMain.handle("open-dialog", async (_e, _arg) => {
+    return (
+      dialog
+        // フォルダ選択ダイアログを表示する
+        .showOpenDialog(consoleWindow, {
+          properties: ["openDirectory"],
+          title: "フォルダ選択",
+        })
+        .then((result) => {
+          // キャンセルボタンが押されたとき
+          if (result.canceled) return "";
+
+          // 選択されたファイルの絶対パスを返す
+          return result.filePaths[0];
+        })
+    );
   });
 
   // ipcMain.on('update-title', (_e, arg) => {
@@ -43,13 +61,13 @@ const createWindows = () => {
     //   })
     //   .catch((err) => console.log(err));
 
-    backgroundWindow.webContents.openDevTools({ mode: 'detach' });
-    consoleWindow.webContents.openDevTools({ mode: 'detach' });
+    backgroundWindow.webContents.openDevTools({ mode: "detach" });
+    consoleWindow.webContents.openDevTools({ mode: "detach" });
   }
 
-  backgroundWindow.loadFile('dist/background/index.html');
-  consoleWindow.loadFile('dist/console/index.html');
+  backgroundWindow.loadFile("dist/background/index.html");
+  consoleWindow.loadFile("dist/console/index.html");
 };
 
 app.whenReady().then(createWindows);
-app.once('window-all-closed', () => app.quit());
+app.once("window-all-closed", () => app.quit());
--- src/preload.ts
+++ src/preload.ts
@@ -1,5 +1,10 @@
-import { contextBridge, ipcRenderer } from 'electron';
+import { contextBridge, ipcRenderer } from "electron";
 
-contextBridge.exposeInMainWorld('myAPI', {
-  update: (count: number) => ipcRenderer.send('update-title', count),
+contextBridge.exposeInMainWorld("myAPI", {
+  update: (count: number) => ipcRenderer.send("update-title", count),
+  openDialog: async () => ipcRenderer.invoke("open-dialog"),
 });
+
+// contextBridge.exposeInMainWorld("myAPI", {
+//   openDialog: async () => ipcRenderer.invoke("open-dialog"),
+// });
