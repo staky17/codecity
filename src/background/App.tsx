a1bcc779b3ae1c5453d53fc59a6dd1752aff17a9
kei67 <kei.02to.14@gmail.com>
2022-06-21 17:24:35 +0900

    town rotate

--- src/background/App.tsx
+++ src/background/App.tsx
@@ -1,11 +1,85 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 const { myAPI } = window;
 
+import { Canvas } from "@react-three/fiber";
+import * as THREE from "three";
+
 export const App = () => {
+  const createBox = () => {
+    const width = 500;
+    const height = 500;
+    const renderer: any = new THREE.WebGLRenderer({
+      canvas: document.querySelector("#myCanvas") as HTMLCanvasElement,
+    });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(width, height);
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(45, width / height);
+    // camera.position.set(200, 0, +800);
+    camera.position.set(500, 500, 0);
+    camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+    // 箱を作成
+    // const geometry = new THREE.BoxGeometry(400, 400, 400);
+    const material = new THREE.MeshNormalMaterial();
+    // const box = new THREE.Mesh(geometry, material);
+    // scene.add(box);
+
+    const buildings = [];
+    for (let i = -5; i < 5; i++) {
+      for (let k = -5; k < 5; k++) {
+        const height = Math.random() * 100;
+        const geometry = new THREE.BoxGeometry(10, height, 10);
+        const building = new THREE.Mesh(geometry, material);
+
+        building.position.set(10 * i, height / 2, 10 * k);
+        buildings.push(building);
+      }
+    }
+
+    const materialGroup = new THREE.Group();
+    materialGroup.add(...buildings);
+
+    scene.add(materialGroup);
+
+    tick();
+    // 毎フレーム時に実行されるループイベント
+    function tick() {
+      materialGroup.rotation.y += 0.01;
+      // materialGroup.rotation.x -= 0.05;
+      // materialGroup.rotation.z -= 0.01;
+
+      renderer.render(scene, camera);
+      // レンダリング
+      requestAnimationFrame(tick);
+    }
+  };
+
+  useEffect(() => {
+    createBox();
+  }, []);
   return (
-    <div className="container">
-      <h1>This is background</h1>
-    </div>
+    <>
+      <canvas id="myCanvas" />
+    </>
   );
+
+  // return <div ref={mountRef} />;
+  // return (
+  // <div className="container">
+  //   <Canvas>
+  //     <ambientLight intensity={0.1} />
+  //     <directionalLight color="white" position={[0, 0, 5]} />
+  //     <mesh>
+  //       <boxGeometry args={[5, 5, 5]} />
+  //       <meshStandardMaterial color="0x44c2b5" />
+  //     </mesh>
+  //     <mesh>
+  //       <boxGeometry args={[5, 5, 5]} />
+  //       <meshStandardMaterial />
+  //     </mesh>
+  //   </Canvas>
+  // </div>
+  // );
 };
