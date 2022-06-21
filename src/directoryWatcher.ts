1b0483736d98ee56129ade6387ef75362f5aaefb
s-amano <amahaya0831@gmail.com>
2022-06-21 20:43:40 +0900

    update array -> tree

--- src/directoryWatcher.ts
+++ src/directoryWatcher.ts
@@ -1,4 +1,5 @@
 import chokidar from "chokidar";
+const _ = require("lodash");
 
 class DirectoryWatcher {
   private watcher?: chokidar.FSWatcher;
@@ -11,7 +12,18 @@ class DirectoryWatcher {
     });
 
     this.watcher.on("ready", async () => {
-      console.log("ready watching...");
+      const allFiles = this.watcher?.getWatched();
+      if (typeof allFiles === "undefined") {
+        return;
+      }
+      const formattedAllFiles = Object.entries(allFiles)
+        .map(([path, basenames]) => basenames.map((name) => `${path}/${name}`))
+        .flat();
+
+      this.pathString2Tree(formattedAllFiles, function (tree) {
+        console.log("tree: ", JSON.stringify(tree[0]));
+      });
+
       this.watcher?.on("add", (path: string) => {
         console.log(path + " added.");
       });
@@ -23,6 +35,42 @@ class DirectoryWatcher {
       this.watcher.close();
       this.watcher = undefined;
     }
+  }
+
+  pathString2Tree(paths, callBack: (tree) => void): void {
+    var tree = [];
+
+    //ループする！
+    _.each(paths, function (path: string) {
+      // currentLevelを rootに初期化する
+      var pathParts = path.split("/");
+      pathParts.shift();
+      // currentLevelを rootに初期化する
+      var currentLevel = tree;
+
+      _.each(pathParts, function (part: string) {
+        // pathが既存しているかどうかをチェックする
+        var existingPath = _.find(currentLevel, {
+          name: part,
+        });
+
+        if (existingPath) {
+          // Pathはすでにツリー構造に入っているので、追加しない
+          // current levelを下の子供階層に設定し直す
+          currentLevel = existingPath.children;
+        } else {
+          var newPart = {
+            name: part,
+            children: [],
+          };
+
+          currentLevel.push(newPart);
+          currentLevel = newPart.children;
+        }
+      });
+    });
+
+    callBack(tree);
   }
 }
 
