e69c9631ada00db05be5e0ac36e3a8040ab33984
Saji Ayahito <ayahito_saji@outlook.jp>
2022-06-21 22:35:44 +0900

    add material UI

--- src/console/App.tsx
+++ src/console/App.tsx
@@ -1,20 +1,30 @@
 import React, { useState, useEffect } from "react";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
 
 const { myAPI } = window;
 
 export const App = () => {
-  const [folderPath, setFolderPath] = useState();
+  const [folderPath, setFolderPath] = useState("");
 
   const onClickOpenFolderDialog = async () => {
     const folderPathName = await window.myAPI.openDialog();
-    setFolderPath(folderPathName);
+    if (folderPathName !== "") setFolderPath(folderPathName);
   };
   return (
     <div className="container">
-      <button onClick={onClickOpenFolderDialog} id="button">
-        Open folder
-      </button>
-      <p id="text">{folderPath}</p>
+      <TextField
+        label="フォルダパス"
+        InputProps={{
+          value: folderPath,
+          readOnly: true,
+        }}
+        variant="standard"
+        onClick={onClickOpenFolderDialog}
+      />
+      <Button variant="text" onClick={onClickOpenFolderDialog}>
+        フォルダを開く
+      </Button>
     </div>
   );
 };
