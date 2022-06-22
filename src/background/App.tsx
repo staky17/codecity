dbf99607b2b5d8b0e572f3fabb0824969cbabf6f
Saji Ayahito <ayahito_saji@outlook.jp>
2022-06-22 12:15:22 +0900

    view building list on background

--- src/background/App.tsx
+++ src/background/App.tsx
@@ -2,32 +2,47 @@ import React, { useState, useEffect } from "react";
 
 const { myAPI } = window;
 
-type FileInfo = {
-  path: string;
-};
-
 export const App = () => {
-  const [fileInfos, setFileInfos] = useState({});
+  const [fileInfoDict, setFileInfoDict] = useState({} as FileInfoDict);
 
   myAPI.on("reset", () => {
     console.log("街を初期化します");
+    setFileInfoDict({});
   });
 
   myAPI.on("addFile", (_: Electron.IpcRendererEvent, fileInfo: FileInfo) => {
     console.log("建物を追加します", fileInfo);
+    setFileInfoDict((prevFileInfoDict) =>
+      Object.assign({}, prevFileInfoDict, {
+        [fileInfo.path]: fileInfo,
+      } as FileInfoDict)
+    );
   });
 
   myAPI.on("updateFile", (_: Electron.IpcRendererEvent, fileInfo: FileInfo) => {
     console.log("建物を更新します", fileInfo);
+    setFileInfoDict((prevFileInfoDict) =>
+      Object.assign({}, prevFileInfoDict, {
+        [fileInfo.path]: fileInfo,
+      } as FileInfoDict)
+    );
   });
 
   myAPI.on("removeFile", (_: Electron.IpcRendererEvent, fileInfo: FileInfo) => {
     console.log("建物を削除します", fileInfo);
+    setFileInfoDict((prevFileInfoDict) =>
+      (({ [fileInfo.path]: undefined, ...rest }: FileInfoDict) => rest)(
+        prevFileInfoDict
+      )
+    );
   });
 
   return (
     <div className="container">
       <h1>This is background</h1>
+      {Object.keys(fileInfoDict).map((path) => (
+        <div key={path}>{path}</div>
+      ))}
     </div>
   );
 };
