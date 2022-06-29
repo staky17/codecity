6ab72a4686e20d5e90d33a7a7370a500aae4e256
Saji Ayahito <ayahito_saji@outlook.jp>
2022-06-29 14:13:36 +0900

    generate roads

--- src/background/MapGenerator.ts
+++ src/background/MapGenerator.ts
@@ -101,6 +101,53 @@ function crossingSegmentCount(
   return count;
 }
 
+// 与えられた半直線(origin, derection)と線分(vector1とvector2を端点とする)の交点があるか調べる．
+// あればorigin + k * directionのkを返す．
+function crossingPointBetweenSegmentAndHalfLine(
+  origin: Vector2d,
+  direction: Vector2d,
+  vector1: Vector2d,
+  vector2: Vector2d
+): number | null {
+  if (
+    isCrossingSegmentAndLine(origin, origin.add(direction), vector1, vector2) &&
+    direction.x * (vector2.z - vector1.z) -
+      direction.z * (vector2.x - vector1.x) !==
+      0
+  ) {
+    let k =
+      ((origin.z - vector1.z) * (vector2.x - vector1.x) -
+        (origin.x - vector1.x) * (vector2.z - vector1.z)) /
+      (direction.x * (vector2.z - vector1.z) -
+        direction.z * (vector2.x - vector1.x));
+    if (k >= 0) return k;
+  }
+  return null;
+}
+
+// 与えられた半直線(origin, derection)と多角形vectorsの交点があるか調べる．
+// あれば到達までの最短のkを返す
+function crossingHalfLine(
+  origin: Vector2d,
+  direction: Vector2d,
+  vectors: Array<Vector2d>
+): number | null {
+  let mink: number | null = null;
+  let k: number | null = null;
+  for (let i = 0; i < vectors.length; i++) {
+    k = crossingPointBetweenSegmentAndHalfLine(
+      origin,
+      direction,
+      vectors[i],
+      vectors[(i + 1) % vectors.length]
+    );
+    if (k !== null && (mink === null || mink > k)) {
+      mink = k;
+    }
+  }
+  return mink;
+}
+
 // 与えられた領域vectorsが点vectorを含んでいるか判定する．
 function containPoint(vectors: Array<Vector2d>, vector: Vector2d): boolean {
   const infinityPoint = new Vector2d(10 ** 9, 10 ** 9);
@@ -149,6 +196,98 @@ function perimeter(vectors: Array<Vector2d>) {
   return bottomVertices.concat(topVertices.reverse());
 }
 
+// 最も近い点を求める
+function getNeighborhoodVectorIndex(
+  vectors: Array<Vector2d>,
+  vector: Vector2d
+): number {
+  // vectors1とvectors2それぞれで，互いに最も近い頂点番号
+  let v, minv;
+  minv = Infinity;
+  let argmin = -1;
+  for (let i = 0; i < vectors.length; i++) {
+    v = vectors[i].sub(vector).size();
+    if (v < minv) {
+      minv = v;
+      argmin = i;
+    }
+  }
+  return argmin;
+}
+
+// 与えられた2つの凸集合の間を通る直線の通る2点を求める．
+function getSeparateLine(
+  vectors1: Array<Vector2d>,
+  vectors2: Array<Vector2d>,
+  baseVector1: Vector2d,
+  baseVector2: Vector2d
+): [Vector2d, Vector2d] | null {
+  // 各ペアに対して，そのペアのオブジェクト同士の最も近い点の番号
+  let vectors1Neighborhood = getNeighborhoodVectorIndex(vectors1, baseVector2);
+  let vectors2Neighborhood = getNeighborhoodVectorIndex(vectors2, baseVector1);
+
+  // 分離軸の基準点を計算
+  let origin = vectors1[vectors1Neighborhood]
+    .add(vectors2[vectors2Neighborhood])
+    .times(1 / 2);
+
+  // vectors1の中で，vectors2と最も近い点の一つ前の点と後ろの点
+  let nearVectors1Neighborhoods = [
+    (vectors1Neighborhood + vectors1.length - 1) % vectors1.length,
+    (vectors1Neighborhood + 1) % vectors1.length,
+  ];
+
+  // vectors1の中で，vectors2と最も近い点の一つ前の点と後ろの点
+  let nearVectors2Neighborhoods = [
+    (vectors2Neighborhood + vectors2.length - 1) % vectors2.length,
+    (vectors2Neighborhood + 1) % vectors2.length,
+  ];
+
+  let separateLines: Array<[Vector2d, Vector2d]> = [];
+  for (let v1id of nearVectors1Neighborhoods) {
+    let direction = vectors1[v1id].sub(vectors1[vectors1Neighborhood]);
+    if (
+      !isCrossingSegmentAndLine(
+        origin,
+        direction.add(origin),
+        vectors2[vectors2Neighborhood],
+        vectors2[nearVectors2Neighborhoods[0]]
+      ) &&
+      !isCrossingSegmentAndLine(
+        origin,
+        direction.add(origin),
+        vectors2[vectors2Neighborhood],
+        vectors2[nearVectors2Neighborhoods[1]]
+      )
+    )
+      separateLines.push([origin, direction]);
+  }
+  for (let v2id of nearVectors2Neighborhoods) {
+    let direction = vectors2[v2id].sub(vectors2[vectors2Neighborhood]);
+    if (
+      !isCrossingSegmentAndLine(
+        origin,
+        direction.add(origin),
+        vectors1[vectors1Neighborhood],
+        vectors1[nearVectors1Neighborhoods[0]]
+      ) &&
+      !isCrossingSegmentAndLine(
+        origin,
+        direction.add(origin),
+        vectors1[vectors1Neighborhood],
+        vectors1[nearVectors1Neighborhoods[1]]
+      )
+    )
+      separateLines.push([origin, direction]);
+  }
+  if (separateLines.length > 0) {
+    return separateLines.sort(
+      (a, b) => b[1].sub(b[0]).size() - a[1].sub(a[0]).size()
+    )[0];
+  }
+  return null;
+}
+
 // Mapに描写されるものはMapObjectです．
 // 建物や地区の基底クラスです．
 // 建物と地区に共通する部分を書く．
@@ -158,11 +297,19 @@ class MapObject {
   public base: Vector2d;
   public force: Vector2d; // そのMapObjectが現在受けている力ベクトル
 
+  // デバッグ用にマーカーとライン弾けるようにした
+  public debugMarkers: Array<{ point: Vector2d; color?: string }>;
+  public debugLines: Array<{ start: Vector2d; end: Vector2d; color?: string }>;
+
   constructor(name: string, vertices: Array<Vector2d>, basePoint: Vector2d) {
     this.name = name;
     this.vertices = vertices;
     this.base = basePoint;
     this.force = new Vector2d(0, 0);
+
+    // デバッグ用にマーカーとライン引けるようにした
+    this.debugMarkers = [];
+    this.debugLines = [];
   }
 
   area(): number {
@@ -289,6 +436,146 @@ export class District extends MapObject {
     // 頂点の集合から外周を得る
     this.vertices = perimeter(points);
   }
+
+  // 道を生成する
+  generateRoads(): void {
+    const children = this.getChildrenAsList();
+
+    // 各オブジェクトの座標にbaseを追加して基準を合わせる．
+    const childrenVertices: Vector2d[][] = children.map((child) => {
+      return child.vertices.map((vertex) => {
+        return vertex.add(child.base);
+      });
+    });
+
+    // 間に建物や地区が存在しないような2つを列挙する
+    const pairs: Array<[number, number]> = [];
+    for (let i = 0; i < children.length; i++) {
+      for (let j = i + 1; j < children.length; j++) {
+        // 二つのオブジェクトを囲うオブジェクトを計算
+
+        // 他のオブジェクトと重なるかを探索する
+        let neighboring = true;
+        for (let k = 0; k < children.length; k++) {
+          if (
+            i !== k &&
+            j !== k &&
+            crossingSegmentCount(
+              childrenVertices[k],
+              children[i].base,
+              children[j].base
+            ) > 0
+          ) {
+            neighboring = false;
+            break;
+          }
+        }
+        if (neighboring === true) pairs.push([i, j]);
+      }
+    }
+
+    let routeCandidates: Array<{
+      dist1: Vector2d; // base + dist1で目的の場所まで
+      dist2: Vector2d; // base + dist2で目的の場所まで
+      dist1isOuter: boolean; // base + dist1が外周になっているか
+      dist2isOuter: boolean; // base + dist2が外周になっているか
+      base: Vector2d; // スタート地点
+    }> = [];
+    let routes: Array<{ start: Vector2d; end: Vector2d }> = [];
+
+    for (let pair of pairs) {
+      // 分離線は必ず得られる
+      let separateLine = getSeparateLine(
+        childrenVertices[pair[0]],
+        childrenVertices[pair[1]],
+        children[pair[0]].base,
+        children[pair[1]].base
+      );
+      if (separateLine === null) continue; // 得られないことはないはずだが得られなかった場合はなにもしない
+
+      // 半直線とオブジェクトが重ならない
+      // startからendの正数倍方向に向かった半直線
+      let minkPositive: number | null = null;
+      let minkNegative: number | null = null;
+      let kPositive: number | null = null;
+      let kNegative: number | null = null;
+      let kPositiveOuter: number = 0;
+      let kNegativeOuter: number = 0;
+
+      // 外側の座標とぶつかるkを見つける
+      kPositiveOuter =
+        crossingHalfLine(separateLine[0], separateLine[1], this.vertices) || 0;
+      kNegativeOuter =
+        crossingHalfLine(
+          separateLine[0],
+          separateLine[1].times(-1),
+          this.vertices
+        ) || 0;
+
+      // 内部の座標とぶつかるならばその中で最も小さいkを見つける
+      for (let i = 0; i < children.length; i++) {
+        kPositive = crossingHalfLine(
+          separateLine[0],
+          separateLine[1],
+          childrenVertices[i]
+        );
+        if (
+          kPositive !== null &&
+          (minkPositive === null || minkPositive > kPositive)
+        ) {
+          minkPositive = kPositive;
+        }
+        kNegative = crossingHalfLine(
+          separateLine[0],
+          separateLine[1].times(-1),
+          childrenVertices[i]
+        );
+        if (
+          kNegative !== null &&
+          (minkNegative === null || minkNegative > kNegative)
+        ) {
+          minkNegative = kNegative;
+        }
+      }
+      routeCandidates.push({
+        dist1: separateLine[1].times(
+          minkPositive !== null ? minkPositive : kPositiveOuter
+        ),
+        dist2: separateLine[1].times(
+          minkNegative !== null ? -minkNegative : -kNegativeOuter
+        ),
+        dist1isOuter: minkPositive === null,
+        dist2isOuter: minkNegative === null,
+        base: separateLine[0],
+      });
+    }
+
+    // 線分の両端がouterであるものはルートとして確定させる
+    for (let routeCandidate of routeCandidates) {
+      if (routeCandidate.dist1isOuter || routeCandidate.dist2isOuter)
+        routes.push({
+          start: routeCandidate.base.add(routeCandidate.dist1),
+          end: routeCandidate.base.add(routeCandidate.dist2),
+        });
+    }
+    // 線分の片方がouterであるものはルートとして確定させる
+    for (let routeCandidate of routeCandidates) {
+      if (routeCandidate.dist1isOuter && routeCandidate.dist2isOuter)
+        routes.push({
+          start: routeCandidate.base.add(routeCandidate.dist1),
+          end: routeCandidate.base.add(routeCandidate.dist2),
+        });
+    }
+
+    // 各線分を引く
+    for (let route of routes) {
+      this.debugLines.push({
+        start: route.start,
+        end: route.end,
+        color: "#00ff00",
+      });
+    }
+  }
 }
 
 // 建物クラス
@@ -369,10 +656,15 @@ export class MapGenerator {
     // rootから新しく建物を追加したdistrictまでを，逆順でオプティマイズする
     // [rootDistrict, aDistrict, bDistrict, cDistrict]
     for (let d = districts.length - 1; d >= 0; d--) {
+      // デバッグ用の点や線は削除する
+      districts[d].debugMarkers = [];
+      districts[d].debugLines = [];
       // 地区を子(地区)から順番に位置を調整する
       districts[d].optimize();
       // 自分自身の頂点を書き換える．
       districts[d].updateVertices();
+      // 道を引く
+      districts[d].generateRoads();
     }
   }
 }
